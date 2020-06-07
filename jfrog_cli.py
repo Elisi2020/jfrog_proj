2e8e160ebfeb876ee65cc79f6ca288a4d599b4f1
elisi2020 <eli.siesel@gmail.com>
2020-06-07 23:37:59 +0300

    fix auth issue

--- jfrog_cli.py
+++ jfrog_cli.py
@@ -3,133 +3,145 @@ import requests
 import json
 
 
-
 import configparser
 
 
-def read_artifactory_from_config():
+def read_artifactory_from_config(configfile):
     config = configparser.RawConfigParser()
-    config.read('config.properties')
-    config_dict = dict(config.items('CONFIG'))
-    print('config_dict {}'.format(config_dict['artifactory']))
-    return(config_dict['artifactory'])
+    config.read("config.properties")
+    config_dict = dict(config.items("CONFIG"))
+
+    return config_dict["artifactory"]
 
 
 @click.group()
-#@click.option('--username',prompt=True)
-#@click.option('--password',prompt=True,hide_input=True)
-@click.option('--username',default = 'admin')
-@click.option('--password',default = '9odPOM90quGZZjmdtKXC2w')
+@click.option(
+    "--configfile",
+    prompt=True,
+    default="config.properties",
+    help=" Please insert configuration file location if other then default",
+)
+@click.option("--username", prompt=True, default="admin")
+@click.option("--password", prompt=True, default="9odPOM90quGZZjmdtKXC2w")
 @click.pass_context
-def main(ctx,username,password):
-    artifactory = read_artifactory_from_config()
-    print(username,password)
-    #create token using username/password
-#    artifactory = "https://esiesel.jfrog.io/artifactory"  # artifactory URL
-    print('artifactory ='.format(artifactory))
+def main(ctx, username, password, configfile):
+    artifactory = read_artifactory_from_config(configfile)
     api = "/api/security/token"
 
     url = artifactory + api
-    response = requests.get(url, auth=(username, password))
-    print('status code = {}'.format(response.status_code))
+    data = {"username": "admin", "scope": "member-of-groups:admingroup"}
+    response = requests.post(url, auth=(username, password), data=data)
     if response.status_code == 200:
-#        auth_dic = json.loads(response.text)
-        token_id = json.loads(response.text)['tokens'][0]['token_id']
-#        print('token_id = {}'.format(auth_dic['tokens'][0]['token_id']))
-        print('tokenid = {}'.format(token_id))
-
+        access_token = json.loads(response.text)["access_token"]
     else:
-        print(" Can't create token : Sorry: Please try again later")
+        print(
+            "Failed to Create Access Token status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
         exit(1)
 
+    ctx.obj = {"access_token": access_token}
+    ctx.obj.update({"artifactory": artifactory})
+    ctx.obj.update({"username": username})
+    ctx.obj.update({"password": password})
 
-
-
-    ctx.obj = {'token_id': token_id}
-    ctx.obj.update({'artifactory' : artifactory})
-    ctx.obj.update({'username': username})
-    ctx.obj.update({'password': password})
-    print('ctx = tokenid {}'.format(ctx.obj['token_id']))
 
 @main.command()
 @click.pass_context
 def system_ping(ctx):
-    api = '/api/system/ping'
-    url = ctx.obj['artifactory'] + api
-    print('in systemping url = {}'.format(url))
-    headers = { 'Authorization' : 'Bearer' + ctx.obj['token_id']}
-    response = requests.get(url, headers=headers )
-#    response = requests.get(url, auth = (ctx.obj['username'] , ctx.obj['password']) )
-    print('status code = {}'.format(response.status_code))
+    api = "/api/system/ping"
+    url = ctx.obj["artifactory"] + api
+    response = requests.get(url)
+    print("status code = {}".format(response.status_code))
     if response.status_code == 200:
-        print('OK')
+        print(response.text)
     else:
-        print(" Ping Error: Please try again later")
+        print(
+            "Failed to Ping  status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
+
 
 @main.command()
 @click.pass_context
 def system_version(ctx):
 
-    api = '/api/system/version'
-    url = ctx.obj['artifactory'] + api
-    print('in systemping url = {}'.format(url))
-    #headers = {'Authorization': 'Bearer' + ctx.obj['token_id']}
-    #    response = requests.get(url, headers=headers )
-    response = requests.get(url, auth=(ctx.obj['username'], ctx.obj['password']))
-    print('status code = {}'.format(response.status_code))
+    api = "/api/system/version"
+    url = ctx.obj["artifactory"] + api
+    headers = {"Authorization": "Bearer {}".format(ctx.obj["access_token"])}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print('result = '.format(response.text))
-        print(response.text.split(',')[0][1:])
+        print("result = ".format(response.text))
+        print(response.text.split(",")[0][1:])
     else:
-        print(" System Version Api Error: Please try again later")
-        exit(1)
+        print(
+            "Failed to Get System Version status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
+
 
 @main.command()
-@click.option('--new_user',prompt=True)
+@click.option("--new_user", prompt=True)
 @click.pass_context
-def create_user(ctx,new_user):
+def create_user(ctx, new_user):
     api = "/api/security/users/" + new_user
-    url = ctx.obj['artifactory'] + api
+    url = ctx.obj["artifactory"] + api
     params = {}
     params["name"] = new_user
     params["admin"] = "false"
     params["email"] = new_user + "@kuku.com"
     params["password"] = "kukupassword"
     params["Content-Type"] = "application/json"
-    response = requests.put(url, auth=(ctx.obj['username'], ctx.obj['password']), json =params)
+    headers = {"Authorization": "Bearer {}".format(ctx.obj["access_token"])}
+    response = requests.put(url, headers=headers, json=params)
     if response.status_code == 201:
-        print('status code = {} text ={}'.format(response.status_code, response.text))
+        print("status code = {} text ={}".format(response.status_code, response.text))
     else:
-        print('Failed to Create User status code = {} text ={}'.format(response.status_code, response.text))
+        print(
+            "Failed to Create User status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
+
 
 @main.command()
-@click.option('--user_to_delete',prompt=True, help ='Insert Username to be deleted')
+@click.option("--user_to_delete", prompt=True, help="Insert Username to be deleted")
 @click.pass_context
-def delete_user(ctx,user_to_delete):
+def delete_user(ctx, user_to_delete):
     api = "/api/security/users/" + user_to_delete
-    url = ctx.obj['artifactory'] + api
-
-    response = requests.delete(url, auth=(ctx.obj['username'], ctx.obj['password']))
+    url = ctx.obj["artifactory"] + api
+    headers = {"Authorization": "Bearer {}".format(ctx.obj["access_token"])}
+    response = requests.delete(url, headers=headers)
     if response.status_code == 200:
-        print('status code = {} text ={}'.format(response.status_code, response.text))
+        print("status code = {} text ={}".format(response.status_code, response.text))
     else:
-        print('Failed to Delete status code = {} text ={}'.format(response.status_code, response.text))
+        print(
+            "Failed to Delete User status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
+
 
 @main.command()
 @click.pass_context
 def get_storage_info(ctx):
 
-    api = '/api/storageinfo'
-    url = ctx.obj['artifactory'] + api
-    print('in systemping url = {}'.format(url))
-    #headers = {'Authorization': 'Bearer' + ctx.obj['token_id']}
-    #    response = requests.get(url, headers=headers )
-    response = requests.get(url, auth=(ctx.obj['username'], ctx.obj['password']))
+    api = "/api/storageinfo"
+    url = ctx.obj["artifactory"] + api
+    headers = {"Authorization": "Bearer {}".format(ctx.obj["access_token"])}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        print('status code = {} text ={}'.format(response.status_code, response.text))
+        print("status code = {} text ={}".format(response.status_code, response.text))
     else:
-        print('Failed to Get Storage Info status code = {} text ={}'.format(response.status_code, response.text))
+        print(
+            "Failed to Get Storage Info status code = {} text ={}".format(
+                response.status_code, response.text
+            )
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(obj="")
--- test.py
+++ test.py
@@ -1,27 +1,29 @@
 import requests
 import json
 
-#enter credentials
+# enter credentials
 username = "admin"
 password = "9odPOM90quGZZjmdtKXC2w"
-artifactory = "https://esiesel.jfrog.io/artifactory" #artifactory URL
-api = "/api/security/users/admin" #you can change this API URL to any API method you'd like to use
+artifactory = "https://esiesel.jfrog.io/artifactory"  # artifactory URL
+api = "/api/security/users/admin"  # you can change this API URL to any API method you'd like to use
 
 url = artifactory + api
-r = requests.get(url, auth = (username, password)) #this script is only for API methods that use GET
+r = requests.get(
+    url, auth=(username, password)
+)  # this script is only for API methods that use GET
 
-print('status code = {}'.format(r.status_code))
+print("status code = {}".format(r.status_code))
 if r.status_code == 200:
-  print(r.text)
-  #print (r.text.split(',')[0][1:]) response for /api/system/version
+    print(r.text)
+    # print (r.text.split(',')[0][1:]) response for /api/system/version
 else:
-  print ("Fail")
-  response = json.loads(r.content)
-  print (response["errors"])
-  print ("x-request-id : " + r.headers['x-request-id'])
-  print ("Status Code : " + r.status_code)
+    print("Fail")
+    response = json.loads(r.content)
+    print(response["errors"])
+    print("x-request-id : " + r.headers["x-request-id"])
+    print("Status Code : " + r.status_code)
 
-#put
+# put
 api = "/api/security/users/kuku"
 url = artifactory + api
 params = {}
@@ -30,14 +32,17 @@ params["admin"] = "false"
 params["email"] = "kuku@kuku.com"
 params["password"] = "kukupassword"
 params["Content-Type"] = "application/json"
-r = requests.put(url, auth = (username, password),json=params) #this script is only for API methods that use GET
-print('PUT status code = {} text = {}'.format(r.status_code,r.text))
+r = requests.put(
+    url, auth=(username, password), json=params
+)  # this script is only for API methods that use GET
+print("PUT status code = {} text = {}".format(r.status_code, r.text))
 
 
-#delete
+# delete
 api = "/api/security/users/kuku"
 url = artifactory + api
 
-r = requests.delete(url, auth = (username, password)) #this script is only for API methods that use GET
-print('delete status code = {} text ={}'.format(r.status_code,r.text))
-
+r = requests.delete(
+    url, auth=(username, password)
+)  # this script is only for API methods that use GET
+print("delete status code = {} text ={}".format(r.status_code, r.text))
